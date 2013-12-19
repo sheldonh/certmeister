98e25ccd73289122258c7f2cff31c8bccecf70f5
Sheldon Hearn <sheldonh@starjuice.net>
2013-12-19 12:44:57 +0200

    Sign certificate if request valid

--- lib/certmeister/base.rb
+++ lib/certmeister/base.rb
@@ -1,3 +1,6 @@
+require 'time'
+require 'openssl'
+
 module Certmeister
 
   class Base
@@ -5,8 +8,29 @@ module Certmeister
     def initialize(config)
       if config.valid?
         @config = config
+        @authenticator = config.authenticator
+        @ca_cert = config.ca_cert
+        @ca_key = config.ca_key
+        @openssl_digest = config.openssl_digest
       else
         raise RuntimeError.new("invalid config")
+      end
+    end
+
+    def sign(request)
+      if @authenticator.call(request)
+        csr = OpenSSL::X509::Request.new(request[:csr])
+        now = DateTime.now
+        cert = OpenSSL::X509::Certificate.new
+        cert.serial = 0
+        cert.version = 2
+        cert.not_before = now.to_time
+        cert.not_after = (now + (5 * 365)).to_time
+        cert.subject = csr.subject
+        cert.public_key = csr.public_key
+        cert.issuer = @ca_cert.subject
+        cert.sign @ca_key, @openssl_digest.new
+        cert.to_pem
       end
     end
 
--- spec/certmeister_spec.rb
+++ spec/certmeister_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'helpers/certmeister_config_helper'
 
 require 'certmeister'
+require 'openssl'
 
 describe Certmeister do
 
@@ -11,6 +12,34 @@ describe Certmeister do
 
   it "cannot be instantiated with an invalid config" do
     expect { Certmeister.new(Certmeister::Config.new({})) }.to raise_error(RuntimeError, /invalid config/)
+  end
+
+  describe "#sign(request)" do
+
+    let(:valid_request) { {cn: 'axl.hetzner.africa', ip: '127.0.0.1', csr: File.read('fixtures/client.csr'), psk: 'secret'} }
+
+    it "refuses the request if the authenticator declines it" do
+      options = CertmeisterConfigHelper::valid_config_options
+      options[:authenticator] = -> (request) { false }
+      ca = Certmeister.new(Certmeister::Config.new(options))
+      pem = ca.sign(valid_request)
+      expect(pem).to be_nil
+    end
+
+    it "signs a CSR if the authenticator passes the request" do
+      ca = Certmeister.new(CertmeisterConfigHelper::valid_config)
+      pem = ca.sign(valid_request)
+      cert = OpenSSL::X509::Certificate.new(pem)
+      expect(cert.subject.to_s).to match /CN=axl.hetzner.africa/
+      expect(cert.issuer.to_s).to match /CN=Certmeister Test CA/
+    end
+
+    it "does what when the CSR PEM is invalid?"
+
+    it "does what if the request cn does not match the subject of the CSR?"
+
+    it "sets the certificate 'not after' to when?"
+
   end
 
 end
