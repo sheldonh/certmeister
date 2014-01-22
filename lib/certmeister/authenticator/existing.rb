313255b78e8fcdfe952aa9da308216da17e9ef25
Sheldon Hearn <sheldonh@starjuice.net>
2014-01-22 15:59:15 +0200

    Add existing authenticator
    Used to prevent overwriting existing certificates.

---
+++ lib/certmeister/authenticator/existing.rb
@@ -0,0 +1,34 @@
+require 'certmeister/authenticator/response'
+
+module Certmeister
+
+  module Authenticator
+
+    class Existing
+
+      def initialize(store)
+        is_a_store?(store) or raise ArgumentError.new("expected a fetchable store but received a #{store.class}")
+        @store = store
+      end
+
+      def authenticate(request)
+        if request.empty?
+          Certmeister::Authenticator::Response.new(false, "empty request")
+        elsif @store.fetch(request[:cn]).nil?
+          Certmeister::Authenticator::Response.new(true, nil)
+        else
+          Certmeister::Authenticator::Response.new(false, "certificate for cn already exists")
+        end
+      end
+
+      private
+
+      def is_a_store?(store)
+        store.respond_to?(:fetch)
+      end
+
+    end
+
+  end
+
+end
---
+++ spec/certmeister/authenticator/existing_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+require 'certmeister/authenticator/existing'
+require 'certmeister/in_memory_store'
+
+describe Certmeister::Authenticator::Existing do
+
+  subject { Certmeister::Authenticator::Existing.new(Certmeister::InMemoryStore.new) }
+
+  it "must be configured with access to the store" do
+    expect { subject.class.new }.to raise_error(ArgumentError)
+    expect { subject.class.new(Object.new) }.to raise_error(ArgumentError)
+    expect { subject }.to_not raise_error
+  end
+
+  it "demands a request" do
+    expect { subject.authenticate }.to raise_error(ArgumentError)
+  end
+
+  it "refuses to authenticate an empty request" do
+    response = subject.authenticate({})
+    expect(response).to_not be_authenticated
+    expect(response.error).to eql "empty request"
+  end
+
+  context "when the store contains a cert for axl.hetzner.africa" do
+
+    subject { Certmeister::Authenticator::Existing.new(Certmeister::InMemoryStore.new({"axl.hetzner.africa" => "...cert..."})) }
+
+    it "refuses to authenticate a request for axl.hetzner.africa" do
+      response = subject.authenticate(cn: 'axl.hetzner.africa')
+      expect(response).to_not be_authenticated
+      expect(response.error).to match /exists/
+    end
+
+    it "authenticates requests for other common names" do
+      response = subject.authenticate(cn: 'bob.example.com')
+      expect(response).to be_authenticated
+    end
+
+  end
+
+
+
+end
