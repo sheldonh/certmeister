b91293f507ff2e1d7a87ae575664c6359a10ba6f
Sheldon Hearn <sheldonh@starjuice.net>
2014-02-03 11:42:27 +0200

    Work around chick-egg problem with semver2
    Our gemspec uses semver2 to get our version number. Bundler uses our
    gemspec to get our version unumber.  We use bundler to install semver2.

--- lib/certmeister/version.rb
+++ lib/certmeister/version.rb
@@ -1,7 +1,24 @@
-require 'semver'
+begin
 
-module Certmeister
+  require 'semver'
 
-  VERSION = SemVer.find.format("%M.%m.%p%s") unless defined?(VERSION)
-  
+  module Certmeister
+
+    VERSION = SemVer.find.format("%M.%m.%p%s") unless defined?(VERSION)
+    
+  end
+
+rescue LoadError
+
+  $stderr.puts "warning: ignoring missing semver gem for initial bundle"
+  $stderr.puts "warning: please run bundle again to fix certmeister version number"
+
+  module Certmeister
+
+    VERSION = '0'
+
+  end
+
 end
+
+    
