8baef6db8038acb15bfa4ac88a0d16ca308b7f9c
Sheldon Hearn <sheldonh@starjuice.net>
2014-02-04 11:45:14 +0200

    Complete rack application

--- lib/certmeister/rack/app.rb
+++ lib/certmeister/rack/app.rb
@@ -14,26 +14,89 @@ module Certmeister
         req = ::Rack::Request.new(env)
         if req.path_info == '/ping'
           if req.request_method == 'GET'
-            [200, {'Content-Type' => 'text/plain'}, ['PONG']]
+            ok('PONG')
           else
-            [405, {'Content-Type' => 'text/plain'}, ['405 Method Not Allowed']]
+            method_not_allowed
           end
         elsif req.path_info =~ %r{^/certificate/(.+)}
-          params = req.params.tap do |p|
-            p['cn'] = $1
-            p['ip'] = req.ip
-          end
-          response = @ca.sign(params)
-          if response.hit?
-            [303, {'Content-Type' => 'text/plain', 'Location' => req.path_info}, ['303 See Other']]
-          elsif response.denied?
-            [403, {'Content-Type' => 'text/plain'}, ["403 Forbidden (#{response.error})"]]
-          else
-            [500, {'Content-Type' => 'text/plain'}, ["500 Internal Server Error (#{response.error})"]]
+          req.params['cn'] = $1
+          req.params['ip'] = req.ip
+          case req.request_method
+            when 'POST' then sign_action(req)
+            when 'GET' then fetch_action(req)
+            when 'DELETE' then remove_action(req)
+            else method_not_allowed
           end
         else
-          [501, {'Content-Type' => 'text/plain'}, ['501 Not Implemented']]
+          not_implemented
         end
+      end
+
+      private
+
+      def sign_action(req)
+        response = @ca.sign(req.params)
+        if response.hit?
+          redirect(req.path_info)
+        elsif response.denied?
+          forbidden(response.error)
+        else
+          internal_server_error(response.error)
+        end
+      end
+
+      def fetch_action(req)
+        response = @ca.fetch(req.params)
+        if response.hit?
+          ok(response.pem, 'application/x-pem-file')
+        elsif response.miss?
+          not_found
+        elsif response.denied?
+          forbidden(response.error)
+        else
+          internal_server_error(response.error)
+        end
+      end
+
+      def remove_action(req)
+        response = @ca.remove(req.params)
+        if response.hit?
+          ok
+        elsif response.miss?
+          not_found
+        elsif response.denied?
+          forbidden(response.error)
+        else
+          internal_server_error(response.error)
+        end
+      end
+
+      def ok(body = '200 OK', content_type = 'text/plain')
+        [200, {'Content-Type' => content_type}, [body]]
+      end
+
+      def redirect(location)
+        [303, {'Content-Type' => 'text/plain', 'Location' => location}, ['303 See Other']]
+      end
+
+      def not_found
+        [404, {'Content-Type' => 'text/plain'}, ["404 Not Found"]]
+      end
+
+      def forbidden(reason)
+        [403, {'Content-Type' => 'text/plain'}, ["403 Forbidden (#{reason})"]]
+      end
+
+      def method_not_allowed
+        [405, {'Content-Type' => 'text/plain'}, ['405 Method Not Allowed']]
+      end
+
+      def internal_server_error(reason)
+        [500, {'Content-Type' => 'text/plain'}, ["500 Internal Server Error (#{reason})"]]
+      end
+
+      def not_implemented
+        [501, {'Content-Type' => 'text/plain'}, ['501 Not Implemented']]
       end
 
     end
--- spec/certmeister/rack/app_spec.rb
+++ spec/certmeister/rack/app_spec.rb
@@ -9,7 +9,7 @@ describe Certmeister::Rack::App do
   include Rack::Test::Methods
 
   let(:response) { double(Certmeister::Response).as_null_object }
-  let(:ca) { double(Certmeister::Base, sign: response) }
+  let(:ca) { double(Certmeister::Base, sign: response, fetch: response, remove: response) }
   let(:app) { Certmeister::Rack::App.new(ca) }
 
   it "GET /ping always PONGs (although one day we want a health check)" do
@@ -35,25 +35,30 @@ describe Certmeister::Rack::App do
 
   describe "POST /certificate/:cn" do
 
-    it "copies the cn into the params" do
-      post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}
-      expect(ca).to have_received(:sign).with hash_including("cn" => "axl.starjuice.net")
-    end
+    context "parameter handling" do
 
-    it "copies the ip into the params" do
-      post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}, {"REMOTE_ADDR" => "192.168.1.2"}
-      expect(ca).to have_received(:sign).with hash_including("ip" => "192.168.1.2")
-    end
+      let (:response) { Certmeister::Response.hit("...crt...") }
+      before(:each) do
+        post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}, {"REMOTE_ADDR" => "192.168.1.2"}
+      end
 
-    it "passes the form params into the CA" do
-      post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}
-      expect(ca).to have_received(:sign).with hash_including("csr" => "...csr...")
+      it "copies the cn into the params" do
+        expect(ca).to have_received(:sign).with hash_including("cn" => "axl.starjuice.net")
+      end
+
+      it "copies the ip into the params" do
+        expect(ca).to have_received(:sign).with hash_including("ip" => "192.168.1.2")
+      end
+
+      it "passes the form params into the CA" do
+        expect(ca).to have_received(:sign).with hash_including("csr" => "...csr...")
+      end
+
     end
 
     context "on hit" do
 
       let (:response) { Certmeister::Response.hit("...crt...") }
-
       before(:each) do
         post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}
       end
@@ -79,7 +84,6 @@ describe Certmeister::Rack::App do
     context "on denied" do
 
       let (:response) { Certmeister::Response.denied("not enough mojo") }
-
       before(:each) do
         post "/certificate/axl.starjuice.net", {"csr" => "...csr..."}
       end
@@ -112,6 +116,250 @@ describe Certmeister::Rack::App do
 
       it "describes the HTTP status in the body" do
         expect(last_response.body).to eql "500 Internal Server Error (all is lost)"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+  end
+
+  describe "GET /certificate/:cn" do
+
+    context "parameter handling" do
+
+      let (:response) { Certmeister::Response.hit("...crt...") }
+      before(:each) do
+        get "/certificate/axl.starjuice.net", {"psk" => "...secret..."}, {"REMOTE_ADDR" => "192.168.1.2"}
+      end
+
+      it "copies the cn into the params" do
+        expect(ca).to have_received(:fetch).with hash_including("cn" => "axl.starjuice.net")
+      end
+
+      it "copies the ip into the params" do
+        expect(ca).to have_received(:fetch).with hash_including("ip" => "192.168.1.2")
+      end
+
+      it "passes the form params into the CA" do
+        expect(ca).to have_received(:fetch).with hash_including("psk" => "...secret...")
+      end
+
+    end
+
+    context "on hit" do
+
+      let (:response) { Certmeister::Response.hit("...crt...") }
+      before(:each) do
+        get "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 200 OK" do
+        expect(last_response.status).to eql 200
+      end
+
+      it "provides the PEM-encoded X.509 certificate in the body" do
+        expect(last_response.body).to eql "...crt..."
+      end
+
+      it "describes the body as application/x-pem-file" do
+        expect(last_response.headers['Content-Type']).to eql 'application/x-pem-file'
+      end
+
+    end
+
+    context "on miss" do
+
+      let (:response) { Certmeister::Response.miss }
+      before(:each) do
+        get "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 404 Not Found" do
+        expect(last_response.status).to eql 404
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql "404 Not Found"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql "text/plain"
+      end
+
+    end
+
+    context "on denied" do
+
+      let (:response) { Certmeister::Response.denied("your fu is weak") }
+      before(:each) do
+        get "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 403 Forbidden" do
+        expect(last_response.status).to eql 403
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql '403 Forbidden (your fu is weak)'
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+    context "on error" do
+
+      let (:response) { Certmeister::Response.error("i have fallen") }
+
+      before(:each) do
+        get "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 500 Internal Server Error" do
+        expect(last_response.status).to eql 500
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql "500 Internal Server Error (i have fallen)"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+  end
+
+  describe "DELETE /certificate/:cn" do
+
+    context "parameter handling" do
+
+      let (:response) { Certmeister::Response.hit }
+      before(:each) do
+        delete "/certificate/axl.starjuice.net", {"authoritah" => "...warhammer..."}, {"REMOTE_ADDR" => "192.168.1.2"}
+      end
+
+      it "copies the cn into the params" do
+        expect(ca).to have_received(:remove).with hash_including("cn" => "axl.starjuice.net")
+      end
+
+      it "copies the ip into the params" do
+        expect(ca).to have_received(:remove).with hash_including("ip" => "192.168.1.2")
+      end
+
+      it "passes the form params into the CA" do
+        expect(ca).to have_received(:remove).with hash_including("authoritah" => "...warhammer...")
+      end
+
+    end
+
+    context "on hit" do
+
+      let (:response) { Certmeister::Response.hit }
+      before(:each) do
+        delete "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 200 OK" do
+        expect(last_response.status).to eql 200
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql "200 OK"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+    context "on miss" do
+
+      let (:response) { Certmeister::Response.miss }
+      before(:each) do
+        delete "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 404 Not Found" do
+        expect(last_response.status).to eql 404
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql "404 Not Found"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql "text/plain"
+      end
+
+    end
+
+    context "on denied" do
+
+      let (:response) { Certmeister::Response.denied("y u no boss") }
+      before(:each) do
+        delete "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 403 Forbidden" do
+        expect(last_response.status).to eql 403
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql '403 Forbidden (y u no boss)'
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+    context "on error" do
+
+      let (:response) { Certmeister::Response.error("shuffled off this mortal coil") }
+
+      before(:each) do
+        delete "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 500 Internal Server Error" do
+        expect(last_response.status).to eql 500
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql "500 Internal Server Error (shuffled off this mortal coil)"
+      end
+
+      it "describes the body as text/plain" do
+        expect(last_response.headers['Content-Type']).to eql 'text/plain'
+      end
+
+    end
+
+  end
+
+  describe "other verbs on /certificate/:cn" do
+
+    context "(e.g. HEAD)" do
+
+      before(:each) do
+        head "/certificate/axl.starjuice.net"
+      end
+
+      it "returns 405 Method Not Allowed" do
+        expect(last_response.status).to eql 405
+      end
+
+      it "describes the HTTP status in the body" do
+        expect(last_response.body).to eql '405 Method Not Allowed'
       end
 
       it "describes the body as text/plain" do
