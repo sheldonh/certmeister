774adba220faeb0e0372cad3b2b8845abf6498d4
Sheldon Hearn <sheldonh@starjuice.net>
2016-01-05 09:23:14 +0200

    Add key size policy

---
+++ lib/certmeister/policy/key_bits.rb
@@ -0,0 +1,46 @@
+require 'certmeister/policy/response'
+require 'openssl'
+
+module Certmeister
+
+  module Policy
+
+    class KeyBits
+
+      DEFAULT_MIN_KEY_BITS = 4096
+
+      attr_reader :min_key_bits
+
+      def initialize(min_key_bits = DEFAULT_MIN_KEY_BITS)
+        validate_min_key_bits(min_key_bits)
+        @min_key_bits = min_key_bits
+      end
+
+      def authenticate(request)
+        if not request[:pem]
+          Certmeister::Policy::Response.new(false, "missing pem")
+        else
+          cert = OpenSSL::X509::Request.new(request[:pem])
+          pkey = cert.public_key
+          kbits = pkey.n.num_bytes * 8
+          if kbits < @min_key_bits
+            Certmeister::Policy::Response.new(false, "weak key")
+          else
+            Certmeister::Policy::Response.new(true, nil)
+          end
+        end
+      end
+
+      private
+
+      def validate_min_key_bits(min_key_bits)
+        unless min_key_bits.is_a?(Integer)
+          raise ArgumentError.new("invalid minimum key size")
+        end
+      end
+
+    end
+
+  end
+
+end
---
+++ spec/certmeister/policy/key_bits_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+require 'certmeister/policy/key_bits'
+
+describe Certmeister::Policy::KeyBits do
+
+  subject { Certmeister::Policy::KeyBits.new(4096) }
+
+  it "may be configured with a minimum key size in bits" do
+    expect { Certmeister::Policy::KeyBits.new("hamster") }.to raise_error(ArgumentError, "invalid minimum key size")
+    expect { Certmeister::Policy::KeyBits.new(4096) }.to_not raise_error
+  end
+
+  it "defaults to #{Certmeister::Policy::KeyBits::DEFAULT_MIN_KEY_BITS} bits minimum key size" do
+    expect(described_class.new.min_key_bits).to eql Certmeister::Policy::KeyBits::DEFAULT_MIN_KEY_BITS
+  end
+
+  it "demands a request" do
+    expect { subject.authenticate }.to raise_error(ArgumentError)
+  end
+
+  it "refuses to authenticate a request with a missing pem" do
+    response = subject.authenticate({anything: 'something'})
+    expect(response).to_not be_authenticated
+    expect(response.error).to eql "missing pem"
+  end
+
+  it "refuses to authenticate an invalid request" do
+    pem = File.read('fixtures/kbits_1024.csr')
+    response = subject.authenticate({pem: pem})
+    expect(response).to_not be_authenticated
+    expect(response.error).to eql "weak key"
+  end
+
+  it "refuses to authenticate a request for a key with too few bits" do
+    pem = File.read('fixtures/kbits_1024.csr')
+    response = subject.authenticate({pem: pem})
+    expect(response).to_not be_authenticated
+    expect(response.error).to eql "weak key"
+  end
+
+  it "authenticates a request for a key with sufficient bits" do
+    pem = File.read('fixtures/kbits_4096.csr')
+    response = subject.authenticate({pem: pem})
+    expect(response).to be_authenticated
+  end
+
+end
